11890b500d705699f1473f6e2ecc20946d989b95
Antoni Tomaszewski <antek.tomaszewski@gmail.com>
2025-06-03 14:54:47 +0200

    fix: config.py

--- app/config.py
+++ app/config.py
@@ -2,19 +2,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
-    database_url: str
-    environment: str
-    debug: bool
-    secret_key: str
-    app_host: str
-    app_port: int
-    app_reload: bool
+    postgres_user: str = "postgres"
+    postgres_password: str = "postgres"
+    postgres_db: str = "app_db"
+    database_url: str = "postgresql://postgres:postgres@db:5432/app_db"
+    environment: str = "development"
+    debug: bool = True
+    secret_key: str = "26Xtncqvz3RiAr-to0LitviJQB1w3R40rV3laKSmRhI"
+    app_host: str = "0.0.0.0"
+    app_port: int = 8000
+    app_reload: bool = True
 
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
